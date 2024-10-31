10795eb54b145026e44cb4f7ddf563b115fa5edc
Gabriel Fontes <hi@m7.rs>
2024-10-31 12:01:55 -0300

    separar ssh key num arquivo, separar vm.tf

--- main.tf
+++ main.tf
@@ -12,33 +12,3 @@ terraform {
 provider "mgc" {
   region = "br-se1"
 }
-
-resource "mgc_ssh_keys" "key" {
-  name = "chave-do-gabriel"
-  # Altere para a sua chave publica
-  key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDci4wJghnRRSqQuX1z2xeaUR+p/muKzac0jw0mgpXE2T/3iVlMJJ3UXJ+tIbySP6ezt0GVmzejNOvUarPAm0tOcW6W0Ejys2Tj+HBRU19rcnUtf4vsKk8r5PW5MnwS8DqZonP5eEbhW2OrX5ZsVyDT+Bqrf39p3kOyWYLXT2wA7y928g8FcXOZjwjTaWGWtA+BxAvbJgXhU9cl/y45kF69rfmc3uOQmeXpKNyOlTk6ipSrOfJkcHgNFFeLnxhJ7rYxpoXnxbObGhaNqn7gc5mt+ek+fwFzZ8j6QSKFsPr0NzwTFG80IbyiyrnC/MeRNh7SQFPAESIEP8LK3PoNx2l1M+MjCQXsb4oIG2oYYMRa2yx8qZ3npUOzMYOkJFY1uI/UEE/j/PlQSzMHfpmWus4o2sijfr8OmVPGeoU/UnVPyINqHhyAd1d3Iji3y3LMVemHtp5wVcuswABC7IRVVKZYrMCXMiycY5n00ch6XTaXBwCY00y8B3Mzkd7Ofq98YHc="
-}
-
-resource "mgc_virtual_machine_instances" "factorio_server" {
-  name = "factorio-server"
-  machine_type = {
-    name = "BV2-8-40"
-  }
-  image = {
-    name = "cloud-debian-12 LTS"
-  }
-  network = {
-    associate_public_ip = true
-    interface = {
-      security_groups = [{
-        id = "4aa1a237-2d57-439b-bf6a-177ddbace4cb" # grupo podetudo
-      }]
-    }
-  }
-  # Passar nossa chave
-  ssh_key_name = mgc_ssh_keys.key.name
-}
-
-output "ip" {
-  value = mgc_virtual_machine_instances.factorio_server.network.public_address
-}
