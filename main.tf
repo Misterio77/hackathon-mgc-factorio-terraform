e062d69e3f4375ccc685039b1b56a76251d36071
Gabriel Fontes <hi@m7.rs>
2024-10-31 12:01:55 -0300

    usar minha propria chave, para ser mais simples

--- main.tf
+++ main.tf
@@ -13,19 +13,14 @@ provider "mgc" {
   region = "br-se1"
 }
 
-resource "tls_private_key" "key" {
-  algorithm = "ED25519"
-}
-
 resource "mgc_ssh_keys" "key" {
-  name = tls_private_key.key.id
-  key = tls_private_key.key.public_key_openssh
+  name = "chave-do-gabriel"
+  # Altere para a sua chave publica
+  key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDci4wJghnRRSqQuX1z2xeaUR+p/muKzac0jw0mgpXE2T/3iVlMJJ3UXJ+tIbySP6ezt0GVmzejNOvUarPAm0tOcW6W0Ejys2Tj+HBRU19rcnUtf4vsKk8r5PW5MnwS8DqZonP5eEbhW2OrX5ZsVyDT+Bqrf39p3kOyWYLXT2wA7y928g8FcXOZjwjTaWGWtA+BxAvbJgXhU9cl/y45kF69rfmc3uOQmeXpKNyOlTk6ipSrOfJkcHgNFFeLnxhJ7rYxpoXnxbObGhaNqn7gc5mt+ek+fwFzZ8j6QSKFsPr0NzwTFG80IbyiyrnC/MeRNh7SQFPAESIEP8LK3PoNx2l1M+MjCQXsb4oIG2oYYMRa2yx8qZ3npUOzMYOkJFY1uI/UEE/j/PlQSzMHfpmWus4o2sijfr8OmVPGeoU/UnVPyINqHhyAd1d3Iji3y3LMVemHtp5wVcuswABC7IRVVKZYrMCXMiycY5n00ch6XTaXBwCY00y8B3Mzkd7Ofq98YHc="
 }
-
 
 resource "mgc_virtual_machine_instances" "factorio_server" {
   name = "factorio-server"
-  ssh_key_name = mgc_ssh_keys.key.name
   machine_type = {
     name = "BV2-8-40"
   }
@@ -40,6 +35,8 @@ resource "mgc_virtual_machine_instances" "factorio_server" {
       }]
     }
   }
+  # Passar nossa chave
+  ssh_key_name = mgc_ssh_keys.key.name
 }
 
 module "deploy" {
